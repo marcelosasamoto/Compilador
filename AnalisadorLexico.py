2d2bd9fa6fc547def607a9a679b555db9bcb9a9b
marcelo sasamoto <marcelosasamoto@gmail.com>
2019-09-18 18:22:04 -0300

    Corrigindo Alguns erros no analisador

--- AnalisadorLexico.py
+++ AnalisadorLexico.py
@@ -9,19 +9,38 @@ class AnalisadorLexico:
         f.close()
         self.token = ["int","float","String","boolean","True","False","return","if","for", "in","or","and", "==","!=",
                       "<=",">=","!","+","-","*","^","/","%",'"',"=","//","{","}","(",")","[","]","print","input"]
-        self.st_especial = ["@","$","¨","~"]
-
+        self.st_especial = ["@","$","¨","~","#",'\\']
+        self.caractere = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","ç","z","x","c","v","b","n","m",
+                          "Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Ç","Z","X","C","V","B","N","M"]
     def separador(self):
         lnText = []
         for i in range(len(self.texto)):
+            rec = False
+            pt = False
             for x in self.texto[i].split(" "):
-                if x != "":                         #remove o \s e add em um novo vetor. (Linha,tipo, palavra)
+                x = x.replace("\n","")
+                if x != "" or x != "\n":                         #remove o \s e add em um novo vetor. (Linha,tipo, palavra)
                     if x in self.token:
                         lnText.append((i+1,"TOKEN",x))
+                        if x == "//":
+                            rec = True
+                        if x == "print":
+                            pt = True
+                    elif pt:
+                        print("LINHA DO PRINT",i) #alguma logica para o print vai ser necessario
+                        if x[0] == "&":
+                            print("PRINT-Identificador")
+                            lnText.append((i+1,"IDENTIFICADOR",x))
+                    elif rec:
+                        print("Comentario---")
                     elif self.is_number(x):
                         lnText.append((i+1,"TOKEN_NUMERICO",x))
                     else:
-                        lnText.append((i+1,"IDENTIFICADOR",x))
+                        if len(x) >1:
+                            if x[0] in self.caractere:
+                                lnText.append((i+1,"IDENTIFICADOR",x))
+                            else:
+                                lnText.append((i+1,"NÃO ACEITO",x))
         return lnText
 
     def is_number(self,num):
--- README.md
+++ README.md
@@ -1,7 +1,55 @@
-# Meu Compilador
-A interface usar Framework Flask
-Analisador lexico ler o nome e codigo e salva um arquivo com extensão .code e depois analisa o arquivo salvo
+# Compilador
+A interface usa Framework Flask
 
+Para testar, use o terminal com seguinte comando:
+```
+python3 Interface.py 
+```
+# Documentação
+   ### Lexico:
+   - Tokens da palavra resevada:      
+   int, float, String, boolean, True , False , return , if , for ,  in , or , and , == , != ,
+   <= , >= , ! , + , - , * , ^ , / , % , " , = , // , { , } , ( , ) , [ , ] , print , input 
+   - Expressão Regular:
+   Deve ser obrigatoriamente iniciado com a-z ou A-Z podendo conter a-z ou A-Z ou 0-9
+   
+
+- A sintaxe é baseada em “espaços” ou “\s”, onde as palavras reservadas e identificadores devem ser separadas pelo espaços para reconhecer a sintaxe. Ex: int x = 0 (Aceita) | int x=0 (não aceita)
+
+
+### Exemplos da linguagem
+```python
+//Declaração de variaveis
+int x = 0
+float y = 0
+int array = []
+
+//Atribuição
+x = 1
+x = x + 1
+array = 1 2 3 4 5
+array[0] = 0
+
+//Condição
+if x > 0 {
+   //Seu codigo aqui
+}
+
+//Laço de Repetição
+for i in array {
+   //Seu codigo aqui
+}
+
+//Entrada
+input Digite o valor de x &x
+
+//Saida
+print o valor de x= &x
+```
+## Exemplos
+
+* Léxico
+    * ![](https://media.giphy.com/media/XE7obhdNoO0HvFnNEl/giphy.gif)
  ## Implementações
   - [x] Léxico
   - [ ] Sintático
